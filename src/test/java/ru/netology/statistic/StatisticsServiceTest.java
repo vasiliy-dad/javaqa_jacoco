b324af07db7b6ee00125faeb94d07490a09f3a03
vasiliy-dad <dadvasaki@gmail.com>
2025-03-30 18:59:38 +0800

    Add Test if first cell of array not max

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -20,4 +20,19 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldFindMaxIncomeSecondCellMore() {
+        StatisticsService service = new StatisticsService();
+
+
+        long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
+
+
+        long actual = service.findMax(incomesInBillions);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
